0cbbcba542819206641c66b0925e43f7f871936b
juruocjl <3024182971@qq.com>
2024-06-30 22:20:59 +0800

    radix_sort

--- hello_world/main.cu
+++ hello_world/main.cu
@@ -3,10 +3,10 @@
 #include <cuda_runtime.h>
 
 __global__ void hello_world(){
-  printf("Hello from %d\n", threadIdx.x);
+  printf("Hello from %d %d\n", blockIdx.x, threadIdx.x);
 }
 
 signed main(){
-  hello_world<<<1,10>>>();
+  hello_world<<<5, 1024>>>();
   cudaDeviceSynchronize();
 }
--- sort/sort.cu
+++ sort/sort.cu
@@ -58,15 +58,115 @@ void bitonic_sort(std :: vector<int> &nums) {
 }
 #undef BLOCK_SIZE
 
+void radix_sort_cpu(std :: vector<int> &nums) {
+  int n = nums.size();
+  std :: vector<int>nums_(nums.size()),a(1<<16),b(1<<16);
+  Timer t;
+  for(int i = 0; i < n; i++) nums[i] = ((unsigned)nums[i]) ^  0x80000000;
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < (1<<16); j++) {
+      a [j] = 0;
+    }
+    for (int j = 0; j < n; j++) {
+      a[((unsigned)nums[j]) >> (i << 4) & 0xffff]++;
+    }
+    b[0] = 0;
+    for (int j = 1; j < (1<<16); j++) {
+      b[j] = b[j - 1] + a[j - 1];
+    }
+    for (int j = 0; j < n; j++) {
+      nums_[b[((unsigned)nums[j]) >> (i << 4) & 0xffff]++] = nums[j]; 
+    }
+    std :: swap(nums, nums_);
+  }
+  for(int i = 0; i < n; i++) nums[i] = ((unsigned)nums[i]) ^  0x80000000;
+  PrintTime();
+}
+
+
+__global__ void radix_sort_count_kernel(uint *a1, uint *cnt, uint shift) {
+  __shared__ uint tmp[256];
+  if (threadIdx.x < 256) {
+    tmp[threadIdx.x] = 0;
+  }
+  __syncthreads();
+  atomicAdd(&tmp[(a1[blockIdx.x * 1024 + threadIdx.x] >> shift & 0xff)], 1);
+  __syncthreads();
+  if (threadIdx.x < 256) {
+    cnt[blockIdx.x * 256 + threadIdx.x] = tmp[threadIdx.x];
+  }
+}
+__global__ void radix_sort_prefix_kernel(uint *cnt, uint *sum, uint n){
+  int x = threadIdx.x;
+  for (uint i = 1; i < n; i++) {
+    cnt[i * 256 + x] += cnt[(i - 1) * 256 + x];
+  }
+  sum[x] = cnt[(n - 1) * 256 + x];
+  __syncthreads();
+  if (threadIdx.x == 0) {
+    for (int i = 0; i < 255; i++) {
+      sum[i + 1] += sum[i];
+    }
+  }
+    
+}
+__global__ void radix_sort_place_kernel(uint *a1, uint *a2, uint *cnt, uint *sum, int shift){
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  for(int i = 1; i < 256; i++)
+    cnt[x * 256 + i] += sum[i - 1];
+  for (uint i = 1023; ~i; i--) {
+    uint v = a1[x * 1024 + i];
+    a2[--cnt[x * 256 + (v >> shift & 0xff)]] = v;
+  }
+}
+
+void radix_sort(std::vector<int> &nums) {
+  uint n = nums.size(), Bytes = n * sizeof(int);
+  for(uint i = 0; i < n; i++) nums[i] = ((unsigned)nums[i]) ^ 0x80000000;
+  uint *a1, *a2, *cnt, *sum;
+  CHECK(cudaMalloc(&a1, Bytes));
+  CHECK(cudaMalloc(&a2, Bytes));
+  CHECK(cudaMalloc(&cnt, n / 1024 * 256 * sizeof(int)));
+  CHECK(cudaMalloc(&sum, 256 * sizeof(int)));
+  CHECK(cudaMemcpy(a1, nums.data(), Bytes, cudaMemcpyHostToDevice));
+  Timer t;
+  for (uint i = 0; i < 4; i++) {
+    radix_sort_count_kernel<<<n / 1024, 1024>>>(a1, cnt, i * 8);
+    radix_sort_prefix_kernel<<<1, 256>>>(cnt, sum, n / 1024);
+    radix_sort_place_kernel<<<n / 1024, 1>>>(a1, a2, cnt, sum, i * 8);
+    std :: swap(a1, a2);
+  }
+  CHECK(cudaDeviceSynchronize());
+  PrintTime();
+  CHECK(cudaMemcpy(nums.data(), a1, Bytes, cudaMemcpyDeviceToHost));
+  for(uint i = 0; i < n; i++) nums[i] = ((unsigned)nums[i]) ^  0x80000000;
+  CHECK(cudaFree(a1));
+  CHECK(cudaFree(a2));
+  CHECK(cudaFree(cnt));
+  CHECK(cudaFree(sum));
+}
+
+
 signed main(){
-  size_t n = 1 << 30;
+  size_t n = 1 << 25;
   std :: mt19937 rnd(random_device{}());
-  std :: vector<int> A(n);
+  std :: vector<int> A(n),STD,B;
   for (int i = 0; i < n; i++) {
     A[i] = rnd();
   }
-  auto B = A;
-  sort_cpu(A);
+  STD=A;
+  sort_cpu(STD);
+  B = A;
+  bitonic_sort_cpu(B);
+  checkResult(STD,B);
+  B = A;
   bitonic_sort(B);
-  checkResult(A,B);
+  checkResult(STD,B);
+  B = A;
+  radix_sort_cpu(B);
+  checkResult(STD,B);
+  B = A;
+  radix_sort(B);
+  checkResult(STD,B);
+  
 }
