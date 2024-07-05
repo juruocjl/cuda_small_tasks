cffc2e61be68b7b8dea515ad8149272c23702c07
juruocjl <3024182971@qq.com>
2024-07-05 15:36:32 +0800

    mat mul v4

--- mat_mul/mat_mul.cu
+++ mat_mul/mat_mul.cu
@@ -119,8 +119,12 @@ void mat_mul_cub(const float *A_h, const float *B_h, size_t m, size_t n, size_t 
 __global__ void mat_mul_v3_kernel (const float4 *A, const float4 *B, size_t m, size_t n, size_t k, float *C) {
   int subx = threadIdx.x * MAT_SIZE;
   int suby = threadIdx.y * MAT_SIZE;
-  int x = blockIdx.x * BLOCK_SIZE * MAT_SIZE + subx;
-  int y = blockIdx.y * BLOCK_SIZE * MAT_SIZE + suby;
+  int blockx = blockIdx.x * BLOCK_SIZE * MAT_SIZE;
+  int blocky = blockIdx.y * BLOCK_SIZE * MAT_SIZE;
+  int x = blockx + subx;
+  int y = blocky + suby;
+  int tid = threadIdx.x * BLOCK_SIZE + threadIdx.y;
+  //BM*BM  B*B个线程 一个线程跑M*M
   float sum[MAT_SIZE][MAT_SIZE];
   for (int i = 0; i < MAT_SIZE; i++) {
     for (int j = 0; j < MAT_SIZE; j++) {
@@ -131,12 +135,9 @@ __global__ void mat_mul_v3_kernel (const float4 *A, const float4 *B, size_t m, s
     __shared__ float4 As[BLOCK_SIZE * MAT_SIZE][BLOCK_SIZE * MAT_SIZE / 4];
     __shared__ float4 Bs[BLOCK_SIZE * MAT_SIZE][BLOCK_SIZE * MAT_SIZE / 4];
     #pragma unroll
-    for (int a = 0; a < MAT_SIZE; a++) {
-      #pragma unroll
-      for (int b = 0; b < MAT_SIZE; b += 4) {
-        As[subx + a][(suby + b) >> 2] = A[((x + a) * n + i * BLOCK_SIZE * MAT_SIZE + suby + b) >> 2];
-        Bs[suby + a][(subx + b) >> 2] = B[((y + a) * n + i * BLOCK_SIZE * MAT_SIZE + subx + b) >> 2];
-      }
+    for (int b = 0; b < MAT_SIZE * MAT_SIZE; b += 4) {
+      As[tid][b >> 2] = A[((blockx + tid) * n + i * BLOCK_SIZE * MAT_SIZE + b) >> 2];
+      Bs[tid][b >> 2] = B[((blocky + tid) * n + i * BLOCK_SIZE * MAT_SIZE + b) >> 2];
     }
     __syncthreads();
     float4 Ass, Bss;
@@ -145,8 +146,8 @@ __global__ void mat_mul_v3_kernel (const float4 *A, const float4 *B, size_t m, s
       for (int a = 0; a < MAT_SIZE; a++) {
         #pragma unroll
         for (int b = 0; b < MAT_SIZE; b++) {
-          #pragma unroll
           float s = 0;
+          #pragma unroll
           for (int c = 0; c < MAT_SIZE / 4; c++) {
             Ass = As[subx + a][j * MAT_SIZE / 4 + c];
             Bss = Bs[suby + b][j * MAT_SIZE / 4 + c];
@@ -192,11 +193,113 @@ void mat_mul_v3(const float *A_h, const float *B_h, size_t m, size_t n, size_t k
   CHECK(cudaFree(C_d));
   free(B_T_h);
 }
+
+#undef BLOCK_SIZE
+#undef MAT_SIZE
+//v4 : add prefresh
+
+#define BLOCK_SIZE 16
+#define MAT_SIZE 8
+
+__global__ void mat_mul_v4_kernel (const float4 *A, const float4 *B, size_t m, size_t n, size_t k, float *C) {
+  int subx = threadIdx.x * MAT_SIZE;
+  int suby = threadIdx.y * MAT_SIZE;
+  int blockx = blockIdx.x * BLOCK_SIZE * MAT_SIZE;
+  int blocky = blockIdx.y * BLOCK_SIZE * MAT_SIZE;
+  int x = blockx + subx;
+  int y = blocky + suby;
+  int tid = threadIdx.x * BLOCK_SIZE + threadIdx.y;
+  
+  float sum[MAT_SIZE][MAT_SIZE];
+  for (int i = 0; i < MAT_SIZE; i++) {
+    for (int j = 0; j < MAT_SIZE; j++) {
+      sum[i][j] = 0;
+    }
+  }
+  __shared__ float4 As[2][BLOCK_SIZE * MAT_SIZE][MAT_SIZE / 4];
+  __shared__ float4 Bs[2][BLOCK_SIZE * MAT_SIZE][MAT_SIZE / 4];
+  int write_stage_idx = 0;
+  if(tid < BLOCK_SIZE * MAT_SIZE * MAT_SIZE / 4) {
+    int tx = tid / (MAT_SIZE / 4), ty = tid % (MAT_SIZE / 4);
+    As[0][tx][ty] = __ldg(A + (((blockx + tx) * n) >> 2) + ty);
+    Bs[0][tx][ty] = __ldg(B + (((blocky + tx) * n) >> 2) + ty);
+  }
+  
+  __syncthreads();
+  int i = 0;
+  int up = n / MAT_SIZE;
+  do {
+    int load_stage_idx = write_stage_idx;
+    write_stage_idx ^= 1;
+    if (i + 1 < up) {
+      #pragma unroll
+      if(tid < BLOCK_SIZE * MAT_SIZE * MAT_SIZE / 4) {
+        int tx = tid / (MAT_SIZE / 4), ty = tid % (MAT_SIZE / 4);
+        As[write_stage_idx][tx][ty] = __ldg(A + (((blockx + tx) * n + (i + 1) * MAT_SIZE) >> 2) + ty);
+        Bs[write_stage_idx][tx][ty] = __ldg(B + (((blocky + tx) * n + (i + 1) * MAT_SIZE) >> 2) + ty);
+      }
+    }
+    float4 Ass[MAT_SIZE / 4], Bss[MAT_SIZE / 4];
+    #pragma unroll
+    for (int c = 0; c < MAT_SIZE / 4; c++) {
+      #pragma unroll
+      for (int a = 0; a < MAT_SIZE; a++) {
+        Ass[a] = As[load_stage_idx][subx + a][c];
+        Bss[a] = Bs[load_stage_idx][suby + a][c];
+      }
+      #pragma unroll
+      for (int a = 0; a < MAT_SIZE; a++) {
+        float4 Asss = Ass[a];
+        #pragma unroll
+        for (int b = 0; b < MAT_SIZE; b++) {
+          float4 Bsss = Bss[b];
+          sum[a][b] += Asss.x * Bsss.x + Asss.y * Bsss.y + Asss.z * Bsss.z + Asss.w * Bsss.w;
+        }
+      }
+    }
+    __syncthreads();
+    i++;
+  } while(i < up);
+  for (int a = 0; a < MAT_SIZE; a++) {
+    for (int b = 0; b < MAT_SIZE; b++) {
+      C[(x + a) * k + y + b] = sum[a][b];
+    }
+  }
+}
+
+void mat_mul_v4(const float *A_h, const float *B_h, size_t m, size_t n, size_t k, float *C_h) {
+  float *B_T_h = (float*)malloc(n * k * sizeof(float));
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < k; j++) {
+      B_T_h[j * n + i] = B_h[i * k + j];
+    }
+  }
+  float4 *A_d, *B_d;
+  float *C_d;
+  CHECK(cudaMalloc(&A_d, m * n * sizeof(float)));
+  CHECK(cudaMalloc(&B_d, n * k * sizeof(float)));
+  CHECK(cudaMalloc(&C_d, m * k * sizeof(float)));
+  CHECK(cudaMemcpy(B_d, B_T_h, n * k * sizeof(float), cudaMemcpyHostToDevice));
+  CHECK(cudaMemcpy(A_d, A_h, m * n * sizeof(float), cudaMemcpyHostToDevice));
+  Timer t;
+  dim3 block(BLOCK_SIZE, BLOCK_SIZE);
+  dim3 grid(m / BLOCK_SIZE / MAT_SIZE, k / BLOCK_SIZE / MAT_SIZE);
+  mat_mul_v4_kernel<<<grid, block>>>(A_d, B_d, m, n, k, C_d);
+  cudaDeviceSynchronize();
+  CHECK(cudaGetLastError());
+  PrintTime();
+  CHECK(cudaMemcpy(C_h, C_d, m * k * sizeof(float), cudaMemcpyDeviceToHost));
+  CHECK(cudaFree(A_d));
+  CHECK(cudaFree(B_d));
+  CHECK(cudaFree(C_d));
+  free(B_T_h);
+}
+
 #undef BLOCK_SIZE
 #undef MAT_SIZE
 
 signed main(){
-  int m = 1 << 12, n = 1 << 12, k = 1 << 11;
+  int m = 1 << 14, n = 1 << 14, k = 1 << 11;
   float *A, *B, *STD, *OUT;
   A = (float*)malloc(m * n * sizeof(float));
   B = (float*)malloc(n * k * sizeof(float));
@@ -205,14 +308,14 @@ signed main(){
   
   initialData(A, m * n);
   initialData(B, n * k);
-  mat_mul_cpu(A, B, m, n, k, STD);
-  mat_mul_v1(A, B, m, n, k, OUT);
-  checkResult(STD, OUT ,m * k);
-  mat_mul_v2(A, B, m, n, k, OUT);
-  checkResult(STD, OUT ,m * k);
-  mat_mul_cub(A, B, m, n, k, OUT);
-  checkResult(STD, OUT ,m * k);
-  mat_mul_v3(A, B, m, n, k, OUT);
+  // mat_mul_cpu(A, B, m, n, k, STD);
+  // mat_mul_v1(A, B, m, n, k, OUT);
+  // checkResult(STD, OUT ,m * k);
+  // mat_mul_v2(A, B, m, n, k, OUT);
+  // checkResult(STD, OUT ,m * k);
+  mat_mul_cub(A, B, m, n, k, STD);
+  // checkResult(STD, OUT ,m * k);
+  mat_mul_v4(A, B, m, n, k, OUT);
   checkResult(STD, OUT ,m * k);
   free(A);
   free(B);
